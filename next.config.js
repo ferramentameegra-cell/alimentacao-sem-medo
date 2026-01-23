414a57825b4e25b62f279967f1207e222273ba86
Josyas Borba <josyasborba@hotmail.com>
2026-01-23 16:08:47 -0300

    Remover output standalone e usar comando next padrão para Railway

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone', // Para otimizar o build no Railway/Docker
   images: {
     domains: [],
   },
